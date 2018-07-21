402654a4e9642a8e8fd4217e8cecfcac8eb12ab3
kostyashelest <skostya88@gmail.com>
2018-07-22 00:16:07 +0300

    change js script logic

--- widget_key_overtop/js/script.js
+++ widget_key_overtop/js/script.js
@@ -9,7 +9,7 @@ function addJquery() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.onload = function() {
-        star();
+        addStyle();
     }
     script.src = 'js/jquery-3.2.1.min.js';
     head.appendChild(script);
@@ -17,10 +17,30 @@ function addJquery() {
 
 if (window.jQuery) {
     console.log('jquery подключен');
-    star();
+    addStyle();
 }
 
-function star() {
+function addStyle() {
+    console.log('подключаем стили');
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.onload = function() {
+        addScript();
+    }
+    link.href = 'css/style.css';
+    head.appendChild(link);
+}
+
+function addScript() {
+    var body = document.getElementsByTagName('body')[0];
+    var div = document.createElement('div');
+    div.id = 'link-return-up';
+    body.appendChild(div);
+
+    var divUp = document.getElementById('link-return-up');
+    divUp.innerHTML = '<a href="#" id="return-to-top" ><span class="return-to-top">Наверх</span></a>';
+
     console.log('стартуем основной скрипт');
     jQuery("document").ready(function ($) {
         $(window).scroll(function () {
