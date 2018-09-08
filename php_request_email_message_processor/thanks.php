287e9bf2e3b69def5a9984720789fe8495c4bde4
kostyashelest <skostya88@gmail.com>
2018-09-08 14:51:26 +0300

    added if

--- php_request_email_message_processor/thanks.php
+++ php_request_email_message_processor/thanks.php
@@ -17,9 +17,14 @@ if (!isset($_POST['phone']) && empty($_POST['phone']) && !isset($_POST['name']) 
 $to = ''; //Адреса, куда будут приходить письма. две почты указываем через запятую
 $sitename = $_SERVER['SERVER_NAME'];
 
-if (isset($_POST['phone']) && !empty($_POST['phone']) && !isset($_POST['name']) && empty($_POST['name']) && !isset($_POST['message']) && empty($_POST['message']))
+$type  = strip_tags($_POST['type']);
+$name  = strip_tags($_POST['name']);
+$phone  = strip_tags($_POST['phone']);
+$message  = strip_tags($_POST['message']);
+
+if (isset($_POST['name']) && !empty($_POST['name']))
 {
-    $phone  = strip_tags($_POST['phone']);
+    
 // Формирование заголовка письма
     $subject  = "[Заявка с сайта ".$sitename."]";
     $headers  = "From: mail@".$sitename." \r\n";
@@ -27,46 +32,19 @@ if (isset($_POST['phone']) && !empty($_POST['phone']) && !isset($_POST['name']) 
     $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 // Формирование тела письма
     $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка на обратный звонок</h2>\r\n";
-    $msg .= "<p><strong>Телефон:</strong> ".$phone."</p>\r\n";
-    $msg .= "</body></html>";
-// отправка сообщения
-    mail($to, $subject, $msg, $headers);
-}
-elseif (isset($_POST['phone']) && !empty($_POST['phone']) && isset($_POST['name']) && !empty($_POST['name']) && !isset($_POST['message']) && empty($_POST['message']))
-{
-    $name = strip_tags($_POST['name']);
-    $phone  = strip_tags($_POST['phone']);
-// Формирование заголовка письма
-    $subject  = "[Заявка с сайта ".$sitename."]";
-    $headers  = "From: mail@".$sitename." \r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-// Формирование тела письма
-    $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка</h2>\r\n";
-    $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
-    $msg .= "<p><strong>Телефон:</strong> ".$phone."</p>\r\n";
-    $msg .= "</body></html>";
-// отправка сообщения
-    mail($to, $subject, $msg, $headers);
-}
-elseif (isset($_POST['phone']) && !empty($_POST['phone']))
-{
-    $name = strip_tags($_POST['name']);
-    $phone  = strip_tags($_POST['phone']);
-    $message = strip_tags($_POST['message']);
-// Формирование заголовка письма
-    $subject  = "[Заявка с сайта ".$sitename."]";
-    $headers  = "From: mail@".$sitename." \r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-// Формирование тела письма
-    $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка</h2>\r\n";
-    $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
-    $msg .= "<p><strong>Телефон:</strong> ".$phone."</p>\r\n";
-    $msg .= "<p><strong>Сообщение:</strong> ".$message."</p>\r\n";
+    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка:</h2>\r\n";
+    if(isset($_POST['name']) && !empty($_POST['name'])){
+        $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
+    }
+    if(isset($_POST['phone']) && !empty($_POST['phone'])){
+        $msg .= "<p><strong>Имя:</strong> ".$phone."</p>\r\n";
+    }
+    if(isset($_POST['message']) && !empty($_POST['message'])){
+        $msg .= "<p><strong>Имя:</strong> ".$message."</p>\r\n";
+    }
+    if(isset($_POST['type']) && !empty($_POST['type'])){
+        $msg .= "<p><strong>Имя:</strong> ".$type."</p>\r\n";
+    }
     $msg .= "</body></html>";
 // отправка сообщения
     mail($to, $subject, $msg, $headers);
