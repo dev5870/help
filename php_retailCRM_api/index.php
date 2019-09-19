637c89686e4aac804d3f6566c6dd664823d4a8fe
kostyashelest <skostya88@gmail.com>
2019-09-19 14:28:00 +0300

    curl для работы с api retailCRM

---
+++ php_retailCRM_api/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$fio = 'test';
+$phone = '234523452345';
+$id2 = '102';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://demo.retailcrm.ru/api/v5/orders/create?apiKey=Ra7e4R1BkeObr1C7GAgt2dsOHQ2OOtvJ");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+    "site=super-sale2019-ru&order={\"firstName\":\"".$fio."\",\"phone\":\"".$phone."\",\"items\":[{\"offer\":{\"externalId\":\"102\"}}] }"
+
+);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+echo "Ответ на Ваш запрос: " . $server_output;
+
+
